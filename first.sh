7defb8429205af71a26a5a3c0883aa443b0c01a7
Colin Twist <ctwist@hotmail.com>
2020-06-12 20:55:00 -0700

    Updated commit with revised script comments

---
+++ first.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#This is a very basic shell scripot which echo prints "Hi There"
+echo "Hi there"
+exit 22
---
+++ second.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#This simple scripts allows the user to enter a Name etc after the script which
+#becomes $1 or the first argument/value. You do not need to enter anything but is an option.
+#This then gets printed on te screen as part of the echo command.
+ echo "Hi there!"
+ echo "It's good to see you $1!"
+ exit 0
